7b5b4486a9dc376d344f304cc0ab1d155c56839f
Ryan M Smith <rms1000watt@users.noreply.github.com>
2023-07-02 22:30:29 -0700

    Update new-computer.sh

--- new-computer.sh
+++ new-computer.sh
@@ -11,7 +11,7 @@ cp .zshrc ~/.zshrc
 
 # INSTALL BREW
 if ! type -a brew > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
